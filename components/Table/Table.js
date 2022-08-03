1c511b8778bd8fd01a4a3b04aee1817cf5fbd71c
Valentin-Marko <cimpanmarko@yahoo.com>
2022-08-03 18:17:59 +0300

    table changes

--- components/Table/Table.js
+++ components/Table/Table.js
@@ -19,6 +19,7 @@ const TableComponent = ({
   const [tableItems, setTableItems] = useState(1);
 
   const [page, setPage] = useState(1);
+  const [existsNextPage, setExistsNextPage] = useState(true);
 
   const [endList, setEndList] = useState(false);
 
@@ -53,22 +54,24 @@ const TableComponent = ({
 
   useEffect(() => {
     console.log("THE CARGER HISTORY IS:", chargerHistory);
+    chargerHistory.length === 0 || chargerHistory.length < tableItems
+      ? setExistsNextPage(false)
+      : setExistsNextPage(true);
 
     if (chargerHistory.length !== 0) {
       const tableData = chargerHistory.map((item) => [
         item.startDate.split("T")[0],
         item.endDate.split("T")[0],
-        item.startKwh * 1,
-        item.startKwh * 4.2,
+        Math.round(item.startKwh * 1).toFixed(2),
+        Math.round(item.startKwh * 4.2).toFixed(2),
       ]);
       setTableData(tableData);
-    }
+    } else setTableData([]);
   }, [chargerHistory]);
 
   useEffect(() => {
-    console.log("THE TABLE DIMENSIONS are:", tableDimension);
     if (tableDimension) {
-      const itemsInTable = Math.floor(tableDimension / 46);
+      const itemsInTable = Math.floor(tableDimension / 40);
       setTableItems(itemsInTable);
     }
   }, [tableDimension]);
@@ -88,64 +91,56 @@ const TableComponent = ({
 
   const tableHead = ["Date", "Time", "Kw", "Cost"];
 
-  const tableData2 = [
-    ["1", "2", "3", "4"],
-    ["a", "b", "c", "d"],
-    ["1", "2", "3", "456\n789"],
-    ["a", "b", "c", "d"],
-    ["1", "2", "3", "456\n789"],
-    ["a", "b", "c", "d"],
-    ["1", "2", "3", "456\n789"],
-    ["a", "b", "c", "d"],
-  ];
-
   return (
-    <ScrollView contentContainerStyle={{ flexGrow: 1 }} style={{ flex: 1 }}>
-      <View style={style.container}>
-        <View
-          style={{ backgroundColor: "green", flex: 1 }}
-          onLayout={(event) => {
-            const { height } = event.nativeEvent.layout;
-            setTableDimension(height);
-          }}
-        >
-          <Table>
-            <Row data={tableHead} style={style.head} textStyle={style.text} />
-            <Rows
-              data={tableData ? tableData : tableData2}
-              textStyle={style.text}
-            />
-          </Table>
+    <View style={style.container}>
+      {tableData.length === 0 ? (
+        <Text>No data</Text>
+      ) : (
+        <View style={{ flex: 1 }}>
+          <View
+            style={{ backgroundColor: "green", flex: 1 }}
+            onLayout={(event) => {
+              const { height } = event.nativeEvent.layout;
+              setTableDimension(height);
+            }}
+          >
+            <Table>
+              <Row data={tableHead} style={style.head} />
+              <Rows data={tableData} style={style.text} />
+            </Table>
+          </View>
+
+          <View
+            style={{
+              flexDirection: "row",
+              backgroundColor: "red",
+            }}
+          >
+            <View style={{ flex: 1, marginRight: 10 }}>
+              <PillButton
+                text={"Prev"}
+                isSecondary
+                onPressAction={() =>
+                  page === 1 ? setPage(1) : setPage((prev) => prev - 1)
+                }
+              />
+            </View>
+            <View style={{ flex: 1 }}>
+              <PillButton text={`page: ${page}`} />
+            </View>
+            <View style={{ flex: 1, marginLeft: 10 }}>
+              <PillButton
+                text={"Next"}
+                isSecondary
+                onPressAction={() =>
+                  existsNextPage && setPage((prev) => prev + 1)
+                }
+              />
+            </View>
+          </View>
         </View>
-        <View
-          style={{
-            flexDirection: "row",
-            marginTop: 15,
-            backgroundColor: "red",
-          }}
-        >
-          <View style={{ flex: 1, marginRight: 10 }}>
-            <PillButton
-              text={"Prev"}
-              isSecondary
-              onPressAction={() =>
-                page === 1 ? setPage(1) : setPage((prev) => prev - 1)
-              }
-            />
-          </View>
-          <View style={{ flex: 1 }}>
-            <PillButton text={`page: ${page}`} />
-          </View>
-          <View style={{ flex: 1, marginLeft: 10 }}>
-            <PillButton
-              text={"Next"}
-              isSecondary
-              onPressAction={() => setPage((prev) => prev + 1)}
-            />
-          </View>
-        </View>
-      </View>
-    </ScrollView>
+      )}
+    </View>
   );
 };
 
--- components/Table/Table.style.js
+++ components/Table/Table.style.js
@@ -3,7 +3,6 @@ import { StyleSheet } from "react-native";
 export const style = StyleSheet.create({
   tableWrapper: {
     flexDirection: "row",
-    backgroundColor: "red",
   },
   container: { flex: 1 },
   head: { height: 40, backgroundColor: "#f1f8ff" },
