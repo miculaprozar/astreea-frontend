b5932f52b60791aabfb106c262e0257622eb41a0
Valentin-Marko <cimpanmarko@yahoo.com>
2022-08-05 12:23:56 +0300

    adding permision token

--- Views/Permision/Permision.js
+++ Views/Permision/Permision.js
@@ -2,10 +2,39 @@ import React, { useEffect, useState } from "react";
 import { Text, View, Pressable, Image } from "react-native";
 import Button from "../../components/Button/Button";
 import { style } from "./Permision.style";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useNavigation } from "@react-navigation/native";
+import routes from "../../routes";
 
 import Layout from "../../general_components/Layout";
 
-const Permision = () => {
+const Permision = (props) => {
+  const navigation = useNavigation();
+
+  const {
+    SignIn: { name: SignInRoute },
+  } = routes;
+
+  const navigateToSignIn = () => navigation.navigate(SignInRoute);
+
+  const storePermisionAndNavigate = async () => {
+    try {
+      await AsyncStorage.setItem("hasPermision", "Granted");
+      navigateToSignIn();
+    } catch (e) {
+      console.log("THE TOKEN ERROR", e);
+    }
+  };
+
+  const getHasPermision = async () => {
+    const permision = await AsyncStorage.getItem("hasPermision");
+    permision && navigateToSignIn();
+  };
+
+  useEffect(() => {
+    getHasPermision();
+  }, []);
+
   return (
     <Layout>
       <Layout.Header></Layout.Header>
@@ -21,7 +50,12 @@ const Permision = () => {
           easy to read and navigate.
         </Text>
         <Text style={style.textNotes}>Read Privacy Notice</Text>
-        <Button isSecondary={true} text={"CONTINUE"} marginBottom={60} />
+        <Button
+          isSecondary={true}
+          text={"CONTINUE"}
+          marginBottom={60}
+          onPressAction={() => storePermisionAndNavigate()}
+        />
       </Layout.Footer>
     </Layout>
   );
