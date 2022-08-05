df3c6a4ffa7c4de862515072d7f16e63ff8ff47e
Bogdan Falk <bogdan37falk@gmail.com>
2022-08-05 10:16:47 +0300

    Added toFixed(2) to amountSpent and energyDelivery in Details

--- Views/DeviceDetails/DeviceDetails.js
+++ Views/DeviceDetails/DeviceDetails.js
@@ -1,30 +1,30 @@
-import React, { useEffect, useState } from "react";
-import { Text, View, FlatList, ActivityIndicator } from "react-native";
-import Button from "../../components/Button/Button";
-import PillButton from "../../components/PillButton/PillButton";
-import ChargerButton from "../../components/ChargerButton/ChargerButton";
-import routes from "../../routes";
-import HeaderNavigator from "../../general_components/HeaderNavigator/HeaderNavigator";
-import { apiFactory } from "../../api/index";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { Context } from "../../provider/Provider";
-import Card from "../../components/Card/Card";
-import moment from "moment";
-import DateRangePicker from "react-native-daterange-picker";
-import Table from "../../components/Table/Table";
-import DatePicker from "react-native-modern-datepicker";
-import { format, compareAsc } from "date-fns";
+import React, {useEffect, useState} from 'react';
+import {Text, View, FlatList, ActivityIndicator} from 'react-native';
+import Button from '../../components/Button/Button';
+import PillButton from '../../components/PillButton/PillButton';
+import ChargerButton from '../../components/ChargerButton/ChargerButton';
+import routes from '../../routes';
+import HeaderNavigator from '../../general_components/HeaderNavigator/HeaderNavigator';
+import {apiFactory} from '../../api/index';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {MaterialCommunityIcons} from '@expo/vector-icons';
+import {Context} from '../../provider/Provider';
+import Card from '../../components/Card/Card';
+import moment from 'moment';
+import DateRangePicker from 'react-native-daterange-picker';
+import Table from '../../components/Table/Table';
+import DatePicker from 'react-native-modern-datepicker';
+import {format, compareAsc} from 'date-fns';
 
-import ModalComponent from "../../components/Modal/Modal";
+import ModalComponent from '../../components/Modal/Modal';
 
-import Calendar from "../../general_components/Calendar/Calendar";
+import Calendar from '../../general_components/Calendar/Calendar';
 
-import { style } from "./DeviceDetails.style";
+import {style} from './DeviceDetails.style';
 
-import Layout from "../../general_components/Layout";
+import Layout from '../../general_components/Layout';
 const DeviceDetails = (props) => {
-  const { navigation } = props;
+  const {navigation} = props;
   const {
     route: {
       params: {
@@ -38,16 +38,16 @@ const DeviceDetails = (props) => {
     },
   } = props;
 
-  const { ChargerSettings } = routes;
+  const {ChargerSettings} = routes;
   const [token, setToken] = useState(null);
   const [totalCharge, setTotalCharge] = useState(null);
 
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
 
-  const theDate = format(new Date(), "LLLL");
+  const theDate = format(new Date(), 'LLLL');
 
-  console.log("THE DATE IS:", theDate);
+  console.log('THE DATE IS:', theDate);
 
   const [modalVisible, setModalVisible] = useState(false);
 
@@ -63,19 +63,19 @@ const DeviceDetails = (props) => {
 
   const getData = async () => {
     try {
-      const tokenValue = await AsyncStorage.getItem("token");
+      const tokenValue = await AsyncStorage.getItem('token');
       setToken(tokenValue);
       if (tokenValue !== null) {
         // value previously stored
       }
     } catch (e) {
-      console.log("ERROR IN READING", e);
+      console.log('ERROR IN READING', e);
       // error reading value
     }
   };
 
   useEffect(() => {
-    console.log("Charging Props:", isChargingProp);
+    console.log('Charging Props:', isChargingProp);
 
     setIsCharging(isChargingProp);
   }, [isChargingProp]);
@@ -101,10 +101,10 @@ const DeviceDetails = (props) => {
               endKwh:
                 startStopData.endKwh + demoGetEndKwh(startDate, new Date()),
             },
-            token
+            token,
           );
       } else {
-        console.log("endKwh", startStopData.endKwh);
+        console.log('endKwh', startStopData.endKwh);
         await apiFactory().data.device().startStopCharging(
           {
             chargerId: chargerId,
@@ -113,14 +113,14 @@ const DeviceDetails = (props) => {
             power: 1,
             startKwh: startStopData.endKwh,
           },
-          token
+          token,
         );
       }
       token && getChargerTotalData(token);
       setIsCharging(!isCharging);
       setTriggerRefresh(false);
     } catch (e) {
-      console.log("ERROR IN START STOP CHARGING", e.response.data);
+      console.log('ERROR IN START STOP CHARGING', e.response.data);
     }
   };
 
@@ -132,7 +132,7 @@ const DeviceDetails = (props) => {
       setTotalCharge(totalCharge);
       console.log(totalCharge);
     } catch (e) {
-      console.log("the eeeee is ", e.response.data.message);
+      console.log('the eeeee is ', e.response.data.message);
     }
   };
 
@@ -161,9 +161,9 @@ const DeviceDetails = (props) => {
 
   useEffect(() => {
     if (startDate && endDate) {
-      const requestStartDate = moment(startDate).format("YYYY-MM-DD");
-      const requestEndDate = moment(endDate).format("YYYY-MM-DD");
-      getChargerTotalData(token, { requestStartDate, requestEndDate });
+      const requestStartDate = moment(startDate).format('YYYY-MM-DD');
+      const requestEndDate = moment(endDate).format('YYYY-MM-DD');
+      getChargerTotalData(token, {requestStartDate, requestEndDate});
 
       setTimeout(() => {
         SetIsCalendarOpen(false);
@@ -173,7 +173,7 @@ const DeviceDetails = (props) => {
     }
   }, [startDate, endDate]);
 
-  const actualDate = format(new Date(2022, 7), "LLLL");
+  const actualDate = format(new Date(2022, 7), 'LLLL');
 
   return (
     <>
@@ -188,7 +188,7 @@ const DeviceDetails = (props) => {
             open={isCalendarOpen}
             presetButtons={true}
           >
-            <Text style={{ display: "none" }}></Text>
+            <Text style={{display: 'none'}}></Text>
           </DateRangePicker>
 
           <Layout>
@@ -197,7 +197,7 @@ const DeviceDetails = (props) => {
             </Layout.Header>
             <Layout.Body>
               <View style={style.tittleButtonWrapper}>
-                <View style={{ flex: 2 }}>
+                <View style={{flex: 2}}>
                   <Text style={style.title}> {name}</Text>
                 </View>
                 <View
@@ -206,7 +206,7 @@ const DeviceDetails = (props) => {
                   }}
                 >
                   <PillButton
-                    text={"Settings"}
+                    text={'Settings'}
                     isSecondary
                     onPressAction={navigateToChargerSettings}
                   />
@@ -215,7 +215,7 @@ const DeviceDetails = (props) => {
               {!isCharging ? (
                 <>
                   <View
-                    style={{ ...style.tittleButtonWrapper, marginBottom: 10 }}
+                    style={{...style.tittleButtonWrapper, marginBottom: 10}}
                   >
                     <View
                       style={{
@@ -227,8 +227,8 @@ const DeviceDetails = (props) => {
                         onPressAction={() => setModalVisible(true)}
                       />
                     </View>
-                    <View style={{ flex: 1 }}></View>
-                    <View style={{ flex: 1 }}></View>
+                    <View style={{flex: 1}}></View>
+                    <View style={{flex: 1}}></View>
                   </View>
 
                   <Table
@@ -249,40 +249,40 @@ const DeviceDetails = (props) => {
                     size={350}
                     color="green"
                     style={{
-                      marginLeft: "auto",
-                      marginRight: "auto",
+                      marginLeft: 'auto',
+                      marginRight: 'auto',
                       marginTop: 30,
                     }}
                   />
                 </View>
               )}
             </Layout.Body>
-            <Layout.Footer style={{ flex: 2, backgroundColor: "red" }}>
+            <Layout.Footer style={{flex: 2, backgroundColor: 'red'}}>
               {totalCharge && (
                 <Card
                   isCharging={false}
                   details={true}
-                  price={totalCharge.ammountSpent}
-                  kwh={totalCharge.energyDelivered}
-                  name={isCharging ? "Charging" : "Total"}
+                  price={totalCharge.ammountSpent.toFixed(2)}
+                  kwh={totalCharge.energyDelivered.toFixed(2)}
+                  name={isCharging ? 'Charging' : 'Total'}
                   hourMinutes={`${Math.floor(
-                    (totalCharge.chargeDuration % 86400000) / 3600000
+                    (totalCharge.chargeDuration % 86400000) / 3600000,
                   )} H, ${Math.round(
-                    ((totalCharge.chargeDuration % 86400000) % 3600000) / 60000
+                    ((totalCharge.chargeDuration % 86400000) % 3600000) / 60000,
                   )} M`}
                 />
               )}
               <View style={style.tittleButtonWrapper}>
-                <View style={{ flex: 1 }}>
+                <View style={{flex: 1}}>
                   <ChargerButton
-                    text={"Schedule"}
+                    text={'Schedule'}
                     marginRight={10}
                     isSecondary={true}
                   />
                 </View>
-                <View style={{ flex: 1 }}>
+                <View style={{flex: 1}}>
                   <ChargerButton
-                    text={isCharging ? "Stop" : "Start"}
+                    text={isCharging ? 'Stop' : 'Start'}
                     marginLeft={10}
                     isDanger={isCharging}
                     onPressAction={() => StartStopCharging()}
@@ -300,7 +300,7 @@ const DeviceDetails = (props) => {
           </Layout>
         </>
       ) : (
-        <ActivityIndicator size={"large"} color={"#ff6400"}></ActivityIndicator>
+        <ActivityIndicator size={'large'} color={'#ff6400'}></ActivityIndicator>
       )}
     </>
   );
