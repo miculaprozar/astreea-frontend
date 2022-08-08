af094a3e295b4cd5a1ff92fe949f73bf3b19edba
Valentin-Marko <69463262+Valentin-Marko@users.noreply.github.com>
2022-08-08 00:56:59 -0700

    adding pillBUtton and new searchInput (#5)

--- Views/Home/Home.js
+++ Views/Home/Home.js
@@ -1,20 +1,20 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import React, { useEffect, useState } from 'react';
-import { ScrollView, View } from 'react-native';
-import useWebSocket, { ReadyState } from 'react-native-use-websocket';
-import Button from '../../components/Button/Button';
-import ChargerCard from '../../components/Card/ChargerCard';
-import PillButton from '../../components/PillButton/PillButton';
-import SearchInput from '../../components/SearchInput/SearchInput';
-import HeaderNavigator from '../../general_components/HeaderNavigator/HeaderNavigator';
-import Layout from '../../general_components/Layout';
-import routes from '../../routes';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useEffect, useState } from "react";
+import { ScrollView, View } from "react-native";
+import useWebSocket, { ReadyState } from "react-native-use-websocket";
+import Button from "../../components/Button/Button";
+import ChargerCard from "../../components/Card/ChargerCard";
+import PillButton from "../../components/PillButton/PillButton";
+import SearchInput from "../../components/SearchInput/SearchInput";
+import HeaderNavigator from "../../general_components/HeaderNavigator/HeaderNavigator";
+import Layout from "../../general_components/Layout";
+import routes from "../../routes";
 
 import {
   hourMinutesRenderer,
   kwhRenderer,
   priceRenderer,
-} from '../../helpers/formatFunctions';
+} from "../../helpers/formatFunctions";
 
 const Home = (props) => {
   const { navigation } = props;
@@ -25,10 +25,10 @@ const Home = (props) => {
   const [chargers, setChargers] = useState(null);
   const [getDevices, setGetDevices] = useState(null);
   const [filterChargers, setFilterChargers] = useState(0);
-  const [searchfield, setSearchfield] = useState('');
+  const [searchfield, setSearchfield] = useState("");
 
   // WEBSOCKET CONNECTION
-  const [socketUrl] = React.useState('ws://164.92.234.83:6003');
+  const [socketUrl] = React.useState("ws://164.92.234.83:6003");
   const socketMessageHistory = React.useRef([]);
   const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl, {
     retryOnError: true,
@@ -38,14 +38,14 @@ const Home = (props) => {
     reconnectInterval: 10000,
     reconnectAttempts: Infinity,
     onClose: () => {
-      console.log('Socket closed');
+      console.log("Socket closed");
     },
     onError: (error) => {
-      if (!error?.message?.includes('Failed to connect'))
-        console.log('Socket error', error);
+      if (!error?.message?.includes("Failed to connect"))
+        console.log("Socket error", error);
     },
     onOpen: () => {
-      console.log('Socket opened');
+      console.log("Socket opened");
     },
   });
   socketMessageHistory.current = React.useMemo(
@@ -79,9 +79,9 @@ const Home = (props) => {
           if (readyState === ReadyState.OPEN && token) {
             sendMessage(
               JSON.stringify({
-                method: 'GetKnownDevices',
+                method: "GetKnownDevices",
                 token: token,
-                user: 'Tudor',
+                user: "Tudor",
               })
             );
           } else {
@@ -90,12 +90,12 @@ const Home = (props) => {
         }, 1000)
       );
     } else if (readyState === ReadyState.CONNECTING && token && canMessage) {
-      console.log('Connecting Socket...');
+      console.log("Connecting Socket...");
     } else if (readyState === ReadyState.CLOSING && token && canMessage) {
-      console.log('Closing Socket...');
+      console.log("Closing Socket...");
       clearInterval(getDevices);
     } else if (readyState === ReadyState.CLOSED && token && canMessage) {
-      console.log('Closed Socket...');
+      console.log("Closed Socket...");
       clearInterval(getDevices);
     } else clearInterval(getDevices);
   };
@@ -106,7 +106,7 @@ const Home = (props) => {
   }, [readyState, canMessage]);
 
   const getToken = async () => {
-    const token = await AsyncStorage.getItem('token');
+    const token = await AsyncStorage.getItem("token");
     token && setToken(token);
   };
 
@@ -142,22 +142,18 @@ const Home = (props) => {
       </Layout.Header>
       <Layout.Body>
         <SearchInput setSearchfield={setSearchfield} />
-        <View style={{ flexDirection: 'row', marginBottom: 20, marginTop: 10 }}>
-          <View style={{ flex: 1 }}>
-            <PillButton
-              isSecondary={filterChargers && true}
-              text={'Public'}
-              onPressAction={() => setFilterChargers(0)}
-            />
-          </View>
-          <View style={{ flex: 2 }}>
-            <PillButton
-              isSecondary={!filterChargers && true}
-              text={'My chargers'}
-              marginLeft={15}
-              onPressAction={() => setFilterChargers(1)}
-            />
-          </View>
+        <View style={{ flexDirection: "row", marginBottom: 20, marginTop: 10 }}>
+          <PillButton
+            isSecondary={filterChargers && true}
+            text={"Public"}
+            onPressAction={() => setFilterChargers(0)}
+          />
+          <PillButton
+            isSecondary={!filterChargers && true}
+            text={"My chargers"}
+            marginLeft={15}
+            onPressAction={() => setFilterChargers(1)}
+          />
           <View style={{ flex: 2 }}></View>
         </View>
         <ScrollView>
@@ -181,8 +177,8 @@ const Home = (props) => {
                     item.price,
                     item.currency
                   )}
-                  key={'charger_' + index}
-                  charger={item}
+                  key={"charger_" + index}
+                  statusName={item.state}
                   onClick={() => navigateToDeviceAction(item)}
                 />
               ))}
@@ -190,7 +186,7 @@ const Home = (props) => {
       </Layout.Body>
       <Layout.Footer>
         <Button
-          text={'Add new charger'}
+          text={"Add new charger"}
           marginTop={10}
           onPressAction={navigateToAddDevice}
         />
--- components/SearchInput/SearchInput.js
+++ components/SearchInput/SearchInput.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
-import { TextInput, View } from "react-native";
+import { TextInput, View, Image } from "react-native";
 import { style } from "./SearchInput.style";
 
 const SearchInput = ({ setSearchfield }) => {
   return (
-    <TextInput
-      placeholder={"Search"}
-      style={style.input}
-      onChangeText={(text) => setSearchfield(text)}
-    />
+    <View style={style.wrapper}>
+      <TextInput
+        placeholder={"Search"}
+        style={style.input}
+        onChangeText={(text) => setSearchfield(text)}
+      ></TextInput>
+      <Image style={style.image} source={require("../../assets/search.png")} />
+    </View>
   );
 };
 
