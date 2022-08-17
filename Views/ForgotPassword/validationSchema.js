7843db2e8648e4287d2be9c2159e3776826678f4
Valentin-Marko <cimpanmarko@yahoo.com>
2022-08-17 13:20:49 +0300

    adding first letter to lowerCase validation to ForgotPassword

--- Views/ForgotPassword/validationSchema.js
+++ Views/ForgotPassword/validationSchema.js
@@ -3,6 +3,16 @@ import * as yup from "yup";
 const validationSchema = yup.object().shape({
   email: yup
     .string()
+    .test(
+      "firstLetterToLower",
+      "First email letter must be lower case",
+      function (value) {
+        if (value) {
+          const firstLetter = value[0];
+          return firstLetter[0].toLowerCase() === firstLetter[0];
+        } else return true;
+      }
+    )
     .required("Email-ul este obligatoriu")
     .email("Email incomplet"),
 });
