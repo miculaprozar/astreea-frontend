bf1f3f2eac7ffdb0e21a645fba39af3417232c63
Valentin Gorcea <valentingorcea@gmail.com>
2022-09-14 14:02:50 +0300

    add reset password flow and request for adb2c

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ import Permision from "./Views/Permision/Permision";
 import React, { useEffect, useState } from "react";
 import * as WebBrowser from 'expo-web-browser';
 import base64 from 'react-native-base64'
-import { ADB2C_TENANT, ADB2C_POLICY, ADB2C_CLIENT, ADB2C_REDIRECT_URI, ADB2C_POLICY_EDIT_PROFILE } from "./api/utils/consts.js";
+import { ADB2C_TENANT, ADB2C_POLICY, ADB2C_CLIENT, ADB2C_REDIRECT_URI, ADB2C_POLICY_EDIT_PROFILE, ADB2C_POLICY_PASSWORD_RESET } from "./api/utils/consts.js";
 import {
   JsonHubProtocol,
   HubConnectionState,
@@ -78,16 +78,14 @@ export default function App() {
     },
   };
   const getCodeAuth = async () => {
-    let codeResponse = await WebBrowser.openAuthSessionAsync(`https://${ADB2C_TENANT}.b2clogin.com/${ADB2C_TENANT}.onmicrosoft.com/${ADB2C_POLICY}/oauth2/v2.0/authorize?client_id=${ADB2C_CLIENT}&response_type=code+id_token&redirect_uri=${ADB2C_REDIRECT_URI}&response_mode=query&scope=openid`, ADB2C_REDIRECT_URI); //sign-in & sign-up
+    let codeResponse = await WebBrowser.openAuthSessionAsync(`https://${ADB2C_TENANT}.b2clogin.com/${ADB2C_TENANT}.onmicrosoft.com/${ADB2C_POLICY}/oauth2/v2.0/authorize?client_id=${ADB2C_CLIENT}&response_type=code+id_token&redirect_uri=${ADB2C_REDIRECT_URI}&response_mode=query&scope=openid`, ADB2C_REDIRECT_URI); // sign-in & sign-up
     let code = getSearchParamFromURL(codeResponse.url, 'code')
     let tokenResponse = await axios.post(`https://${ADB2C_TENANT}.b2clogin.com/${ADB2C_TENANT}.onmicrosoft.com/${ADB2C_POLICY}/oauth2/v2.0/token?grant_type=authorization_code&client_id=${ADB2C_CLIENT}&code=${code}&claim=given_name&claim=family_name&claim=idp_access_token`); // get token
-    let tokenId = tokenResponse.data.id_token;
-    console.log(tokenResponse.data)
-    let decoded = base64.decode(tokenResponse.data.profile_info);
-    console.log(decoded)
+    tokenResponse.data.id_token;
+    base64.decode(tokenResponse.data.profile_info);
     //await WebBrowser.openAuthSessionAsync(`https://${ADB2C_TENANT}.b2clogin.com/${ADB2C_TENANT}.onmicrosoft.com/${ADB2C_POLICY_EDIT_PROFILE}/oauth2/v2.0/authorize?client_id=${ADB2C_CLIENT}&response_type=code+id_token&redirect_uri=${ADB2C_REDIRECT_URI}&response_mode=query&scope=openid`, ADB2C_REDIRECT_URI);  //edit profile
-    //let logoutResponse = await WebBrowser.openAuthSessionAsync(`https://${ADB2C_TENANT}.b2clogin.com/${ADB2C_TENANT}.onmicrosoft.com/${ADB2C_POLICY}/oauth2/v2.0/logout?post_logout_redirect_uri=${ADB2C_REDIRECT_URI}`, ADB2C_REDIRECT_URI); // logout
-    //console.log("====>", logoutResponse)
+    //await WebBrowser.openAuthSessionAsync(`https://${ADB2C_TENANT}.b2clogin.com/${ADB2C_TENANT}.onmicrosoft.com/${ADB2C_POLICY}/oauth2/v2.0/logout?post_logout_redirect_uri=${ADB2C_REDIRECT_URI}`, ADB2C_REDIRECT_URI); // logout
+    //await WebBrowser.openAuthSessionAsync(`https://${ADB2C_TENANT}.b2clogin.com/${ADB2C_TENANT}.onmicrosoft.com/${ADB2C_POLICY_PASSWORD_RESET}/oauth2/v2.0/authorize?client_id=${ADB2C_CLIENT}&response_type=code+id_token&redirect_uri=${ADB2C_REDIRECT_URI}&response_mode=query&scope=openid`, ADB2C_REDIRECT_URI); // reset password
   }
 
   useEffect(() => {
