1f0204c35322c62d4767c714e1eebc99faa1b7be
Valentin Gorcea <valentingorcea@gmail.com>
2022-09-27 17:45:46 +0300

    fix back to good configuration for adb2c

--- components/AuthWrapper/AuthProvider.js
+++ components/AuthWrapper/AuthProvider.js
@@ -57,27 +57,22 @@ const AuthProvider = (props) => {
       { showInRecents: true }
     ); // sign-in & sign-up
     let code;
-    console.log(codeResponse)
     try {
       code = getSearchParamFromURL(codeResponse.url, "code");
     } catch (e) {
-      console.log(e);
-      // initAuth();
+      initAuth();
     }
-    // console.log(`https://${ADB2C_TENANT}.b2clogin.com/${ADB2C_TENANT}.onmicrosoft.com/${ADB2C_POLICY}/oauth2/v2.0/token?grant_type=authorization_code&client_id=${ADB2C_CLIENT}&code=${code}&claim=given_name&claim=family_name`)
+
     let tokenResponse = await axios.post(
-      `https://astreeacharger.b2clogin.com/astreeacharger.onmicrosoft.com/B2C_1_astreeacharger_signin/oauth2/v2.0/token?grant_type=authorization_code&client_id=27c0b46b-a394-470c-9c5b-8005206bdce6&code=eyJraWQiOiJjcGltY29yZV8wOTI1MjAxNSIsInZlciI6IjEuMCIsInppcCI6IkRlZmxhdGUiLCJzZXIiOiIxLjAifQ..xTPdzVmFClEOMNJN.jC_kXW3CCXVLXYfDd6Pg2GfwZeH2xVhvW25ZXL76dpWDY6yKJmLQmErDOTNe1rIJnIlQFte7EkGOs3G24TkR_K2gG_7Qhfkf54ci4nayft1sNUXDYj_GY4-J-suqy6hhyhP1KMJnCbVs5ezt_YPF8behS0XSmO37_DXJME7kouCl1PV_UfYyarvHabPcvytPigrVkgUOruXfo1Uqu9oSnFkY8yKilZy5Xn0FzbMaxy5A6Ji1kFonVJ5q_MtzdEz5HQe3B63AopC6i3NkEnejoWDOCcAdAq9S7zMy8TRoU1AO1NGC3gQYsTV7tRcUxjEWjDo8zEz8BD3Tr7n7KajOIPCcJA95znnnc5Y4yEdzP8Hws3Kg2QrAyuocFEtaCaGOAwkh0PXNgFlTOWTNOZ1m6IKaaRNlbC0-rKJUPApd7ysZ8UfGG8E16BOlh3HX-QWA6LUJJDKo5qzX_HIrC25GsSUk504rH8BiMgdMu4-A3UBq25G5K5p2yAKQEC-mG7JdMaR5iUE_2mz0gNB2cL0tL06kDnsJuqKFpmoeVildXqbFyOdMhQA1mn1baRgQOUMiEPy_H_itXUcijIlgm47qc7Pn9Q1of9ExOltwgw.SYBcI5d7E34hs_1dc_AFHw&claim=given_name&claim=family_name`
+      `https://${ADB2C_TENANT}.b2clogin.com/${ADB2C_TENANT}.onmicrosoft.com/${ADB2C_POLICY}/oauth2/v2.0/token?grant_type=authorization_code&client_id=${ADB2C_CLIENT}&code=${code}&claim=given_name&claim=family_name`
     ); // get token
     let userInfo = {};
     try {
       userInfo = base64.decode(tokenResponse.data.profile_info);
-      console.log(userInfo);
       let name = JSON.parse(userInfo).name;
       setUserName(name);
     } catch (e) {
-      console.log(e);
-
-      // initAuth();
+      initAuth();
     }
 
     var authenticationFunctionUrl =
